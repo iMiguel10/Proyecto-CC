6b684cd946c2c043b04f7da66d562d6a1c88566b
iMiguel10 <imiguel10@hotmail.es>
2019-12-03 12:56:38 +0100

    Automatizar construcción y ejecución de docker close #56

--- tasks.py
+++ tasks.py
@@ -6,6 +6,7 @@ from invoke import task, run
 # Tarea para instalar dependencias
 @task
 def install(ctx):
+    """Instala las dependecias del proyecto"""
     print("Instalando dependencias...")
     ctx.run("pip install -r requirements.txt")
     print("HECHO!")
@@ -13,6 +14,7 @@ def install(ctx):
 # Tarea para ejecutar los tests
 @task
 def test(ctx):
+    """Ejecuta los test del proyecto"""
     with ctx.cd('test/'):
         print("Testeando...")
         ctx.run("pytest -v --cov=./")
@@ -21,16 +23,31 @@ def test(ctx):
 # Tarea para codecov
 @task
 def codecov(ctx):
+    """Sube los resultados de los test de cobertura a codecov"""
     with ctx.cd('test/'):
         ctx.run("codecov")
 
 # Tarea para iniciar el servicio
 @task
-def start(ctx):
+def start(ctx,port=8080):
+    """Lanza el servicio web con gunicorn. Puedes especificar el puerto con -p o --port. Por defecto sera el 8080"""
     with ctx.cd('src/'):
-        ctx.run("gunicorn -b :8080 app:app &")
+        ctx.run("gunicorn -b :"+str(port)+" app:app &")
 
 # Tarea para parar el servicio
 @task
 def stop(ctx):
+    """Para el servicio web. Que se puede lanzar con invoke start [-p/--port <puerto>]"""
     ctx.run("pkill gunicorn")
+
+# Tarea para automatizar la construción del contenedor
+@task
+def buildDocker(ctx, ruta="."):
+    """Construye la imagen docker en la ruta con la opción -r o --ruta. Por defecto la ruta es la actual."""
+    print("docker build -t catalogo-entradas "+ruta)
+
+# Tarea para automatizar la ejecución de la imagen
+@task
+def runDocker(ctx, variables, port=8080):
+    """Ejecuta la imagen docker con las varibales de entorno del fichero que se pasa como opción con -v o --variables. Es necesario especificar el puerto, con -p o --port, si no se utilizó el de por defecto(8080)"""
+    ctx.run("docker run -it --rm --env-file "+variables+" -p 80:"+str(port)+" catalogo-eventos")
